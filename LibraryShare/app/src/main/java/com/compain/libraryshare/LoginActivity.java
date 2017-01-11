342a210996e36d534b8665bcf23d2dbd0f65a88e
doyer.du <jie.du@qq.com>
2017-01-11 10:29:23 +0800

    add login

---
+++ LibraryShare/app/src/main/java/com/compain/libraryshare/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.compain.libraryshare;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by jie.du on 17/1/10.
+ */
+public class LoginActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
